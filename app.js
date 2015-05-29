258ff6ecc8aed8eefc8d8e02fc0d1124dcfb6c4a
ebryant <ebryant@igsenergy.com>
2015-05-29 12:25:18 -0400

    Added loggin and a callback function

--- app.js
+++ app.js
@@ -1,29 +1,26 @@
 var fs = require('fs');
 
 module.exports = {
-    findJSScripts: function (inputFile, outputFile, stringToReplace) {
+    findJSScripts: function (inputFile, outputFile, callback) {
         if(!inputFile) {
-            throw 'Error: No input file specified, should be the first parameter'
+            callback('Error: No input file specified, should be the first parameter');
+        }
+        if(!outputFile) {
+            callback('Error: No output file specified');
         }
 
+        console.log("Parsing " + inputFile + '...');
         fs.readFile(inputFile, 'utf8', function (err, data) {
             if(err) {
-                throw 'Error: There was an error reading file ' + inputFile + '\n' + err;
+                callback('Error: There was an error reading file ' + inputFile + '\n' + err);
             }
             if(!data) {
-
-                throw 'Error: Input file has no contents';
+                callback('Error: Input file has no contents');
             }
 
-            var scripts = getScriptsString(data);
-
-            if(outputFile) {
-                writeToFile(outputFile, scripts, stringToReplace);
-            }
-
-            else {
-                console.log(scripts);
-            }
+            var scriptsResult = getScriptsString(data);
+            console.log('Found ' + scriptsResult.count + ' JS scripts');
+            writeToFile(outputFile, scriptsResult.scripts, callback);
         });
     }
 };
@@ -32,9 +29,11 @@ module.exports = {
 function getScriptsString(data) {
     var scripts = "";
     var scriptTagsRegex = /<script[^>]*src="?([^> "]*)"?[^>]*>/g;
+    var count = 0;
 
     var nextScriptTag = scriptTagsRegex.exec(data);
     while (nextScriptTag) {
+        count++;
         scripts += '"' + nextScriptTag[1] + '"';
         nextScriptTag = scriptTagsRegex.exec(data);
         if(nextScriptTag) {
@@ -42,27 +41,28 @@ function getScriptsString(data) {
         }
     }
 
-    return scripts;
+    return {
+        scripts: scripts,
+        count: count
+    };
 }
 
-function writeToFile(file, scripts, stringToReplace) {
-    if(stringToReplace) {
-        fs.readFile(file, 'utf8', function (err, data) {
+function writeToFile(file, scripts, callback) {
+    console.log("Reading contents of " + file + '...');
+    fs.readFile(file, 'utf8', function (err, data) {
+        if (err) {
+            callback('Error: cannot open output file ' + file + '\n' + err);
+        }
+
+        var result = data.replace(new RegExp('"{{scripts}}"', 'g'), scripts);
+        callback();
+
+        console.log("Writing changes to " + file + '...');
+        fs.writeFile(file, result, 'utf8', function (err) {
             if (err) {
-                throw 'Error: cannot open output file ' + file + '\n' + err;
+                callback('Error: cannot write to output file ' + file + '\n' + err);
             }
-
-            var result = data.replace(new RegExp(stringToReplace, 'g'), scripts);
-
-            fs.writeFile(file, result, 'utf8', function (err) {
-                if (err) {
-                    throw 'Error: cannot write to output file ' + file + '\n' + err;
-                }
-            });
         });
-    } else {
-        throw 'Error: If an output file is provided as the second parameter, then a string to replace must be provided ' +
-        'as the third parameter';
-    }
+    });
 }
 
